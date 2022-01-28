d17484a0142fb3f3b787ab9ff5ea494203284976
Adam Cole <acole9@gmail.com>
2022-01-28 00:36:10 +0000

    use of colors way better

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,6 +7,16 @@ void log(string s) {
     cout << s << endl;
 }
 
+void logFloatArr(float arr[], int arrSize) {
+    cout << "[ ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << to_string(arr[i]);
+        
+        if (i != arrSize - 1) cout << ", ";
+    }
+    cout << " ]" << endl;
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     //    ofBackground(255/20.0, 128/20.0, 1);
@@ -16,25 +26,68 @@ void ofApp::setup(){
     ofHideCursor();
     
     
+    beat.load("circles.mp3");
+    beat.play();
+    
     /* This is stuff you always need.*/
     
-    sampleRate             = 44100; /* Sampling Rate */
-    initialBufferSize    = 512;    /* Buffer Size. you have to fill this buffer with sound*/
-    
+//    sampleRate             = 44100; /* Sampling Rate */
+//    initialBufferSize    = 512;    /* Buffer Size. you have to fill this buffer with sound*/
+//
     
     /* Now you can put anything you would normally put in maximilian's 'setup' method in here. */
     
+//
+//    beat.load(ofToDataPath("beat2.wav"));
+//    beat.getLength();
+//
+//    beat.load(ofToDataPath("circles.mp3"));
+//    beat.getLength();
     
-    beat.load(ofToDataPath("beat2.wav"));
-    beat.getLength();
-    
-    
-    ofSoundStreamSetup(2,0,this, sampleRate, initialBufferSize, 4);/* Call this last ! */
+//    ofSoundStreamSetup(2,0,this, sampleRate, initialBufferSize, 4);/* Call this last ! */
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    ofSoundUpdate();
+//
+//    beat.setVolume(1);
+//
+
+    // (5) grab the fft, and put in into a "smoothed" array,
+    //        by taking maximums, as peaks and then smoothing downward
+    float * val = ofSoundGetSpectrum(nBandsToGet);        // request 128 values for fft
+    for (int i = 0;i < nBandsToGet; i++){
+
+        // let the smoothed value sink to zero:
+        fftSmoothed[i] *= 0.96f;
+
+        // take the max, either the smoothed or the incoming:
+        if (fftSmoothed[i] < val[i]) fftSmoothed[i] = val[i];
+
+    }
+
+    for (int i = 0; i <= rollingAvgSize - 2; i++)
+    {
+        rollingAvgArr[i] = rollingAvgArr[i + 1]; //move all element to the right except last one
+        
+       
+    }
     
+    float fftVal = CLAMP(fftSmoothed[1], 0, 1.0);
+    rollingAvgArr[rollingAvgSize - 1] = fftVal;
+    
+    float fftVal2Total = 0; //CLAMP(fftSmoothed[1], 0, 1.0);
+    int rangeStart = 10;
+    int rangeEnd = 18;
+    for (int i = 10; i < 18; i++) {
+        fftVal2Total += fftSmoothed[i];
+    }
+    float fftVal2Avg = fftVal2Total / (rangeEnd - rangeStart);
+    rollingAvgArr2[rollingAvgSize - 1] = fftVal2Avg;
+
+    
+//    logFloatArr(rollingAvgArr, rollingAvgSize);
 }
 
 //--------------------------------------------------------------
@@ -42,7 +95,6 @@ void ofApp::draw(){
     //    ofFill();
     //    ofSetColor(30, 100);
     //    ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
-    
     
     
     int segments = 2000;
@@ -60,19 +112,46 @@ void ofApp::draw(){
     
     ofNoFill();
     ofSetColor(255,0,0);
+    ofEnableBlendMode(OF_BLENDMODE_ADD);
+    
+    
+    float sum = 0;
+    float sum2 = 0;
+    for (int i = 0; i < rollingAvgSize; i++) {
+        sum += rollingAvgArr[i];
+        sum2 += rollingAvgArr2[i];
+    }
+    float fftAvg = sum / rollingAvgSize;
+    float fftAvg2 = sum2 / rollingAvgSize;
+
+    
     
     for (int j = 0; j < shapeCount; j++) {
         float multYShift = ofMap(j, 0, shapeCount, PI/10.0, 0);
         float multY = ofMap(sin((ofGetElapsedTimef()/9.0)+multYShift), -1.0, 1.0, 495.0, 505.0);
         
         //        float red = 255 * ((float)(shapeCount - j) / (float)shapeCount);
-        float r = ofMap(j, 0.0, shapeCount, radius, -20);
-        float red = ofMap(j, 0.0, shapeCount, 0.0, 160.0);
-        float green = ofMap(j, 0.0, shapeCount, 0.0, 1.0);
-        float blue = ofMap(j, 0.0, shapeCount, 0.0, 1.0);
+        float fftAvgClamped2 = CLAMP(fftAvg2, 0, 0.003);
+
+        float rWave = 10 * sin((ofGetElapsedTimef() * 1.5) + multYShift);
         
+        float rFacotr = ofMap(fftAvgClamped2, 0.0, 0.003, 0.0, 225.0);
+        if (ofGetElapsedTimef() < 9.500) rFacotr = 0;
+//        if (rFacotr < 30) rFacotr = 0;
+        log(to_string(rFacotr));
+
+        float r = ofMap(j, 0.0, shapeCount, radius + rWave, - 20);
+        float red = ofMap(j, 0.0, shapeCount, 0.0, 160.0);
+        float green = ofMap(j, 0.0, shapeCount, 0.0, 1.0) + rFacotr;
+        float blue = ofMap(j, 0.0, shapeCount, 0.0, 1.0) + + rFacotr/2;
+        
+        float maxAlpha = 100 * abs(sin((ofGetElapsedTimef()/10.0) + multYShift));
+       
+        float fftAvgClamped = CLAMP(fftAvg, 0, 1.0);
+
+        float maxAlpha2 = ofMap(fftAvgClamped, 0, 1.0, 0, 150);
         float alphaShift = ofMap(j, 0, shapeCount, PI/3.0, 0);
-        float alpha = ofMap(j, 0.0, shapeCount, -20, 100 * abs(sin((ofGetElapsedTimef()/10.0) + multYShift)));
+        float alpha = ofMap(j, 0.0, shapeCount, -20, maxAlpha2);
         
         if (j == shapeCount - 1) {
             // increase background color brightness based on alpha of "closest" cirlce
@@ -108,53 +187,71 @@ void ofApp::draw(){
     
     //        image.draw(ofGetWidth() / 2 - image.getWidth() / 2, ofGetHeight() / 2 - image.getHeight() / 2);
     //        image.resize(100, 100);
+    
+//    ofSetColor(255, 255, 255,255);
+//    ofRect(600, 300, sample*150, sample*150); /* audio sigs go between -1 and 1. See?*/
+//    ofCircle(200, 300, wave*150);
+    ofEnableAlphaBlending();
+        ofSetColor(255,255,255,100);
+        ofDrawRectangle(100,ofGetHeight()-300,5*128,200);
+    ofDisableAlphaBlending();
+    
+    // draw the fft resutls:
+    ofSetColor(255,255,255,255);
+    
+    float width = (float)(5*128) / nBandsToGet;
+    for (int i = 0;i < nBandsToGet; i++){
+        // (we use negative height here, because we want to flip them
+        // because the top corner is 0,0)
+        ofDrawRectangle(100+i*width,ofGetHeight()-100,width,-(fftSmoothed[i] * 200));
+    }
 }
 
 
 //--------------------------------------------------------------
-void ofApp::audioRequested     (float * output, int bufferSize, int nChannels){
-    
-    for (int i = 0; i < bufferSize; i++){
-        
-        /* Stick your maximilian 'play()' code in here ! Declare your objects in testApp.h.
-         
-         For information on how maximilian works, take a look at the example code at
-         
-         http://www.maximilian.strangeloop.co.uk
-         
-         under 'Tutorials'.
-         
-         */
-        
-        
-        
-        sample=beat.play(0.25, 0, beat.length);
-        wave=sine1.sinebuf(abs(mouseX));/* mouse controls sinewave pitch. we get abs value to stop it dropping
-                                         //                                         delow zero and killing the soundcard*/
-        
-        mymix.stereo(sample + wave, outputs, 0.5);
-        
-        
-        output[i*nChannels    ] = outputs[0]; /* You may end up with lots of outputs. add them here */
-        output[i*nChannels + 1] = outputs[1];
-    }
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::audioReceived     (float * input, int bufferSize, int nChannels){
-    
-    
-    /* You can just grab this input and stick it in a double, then use it above to create output*/
-    
-    for (int i = 0; i < bufferSize; i++){
-        
-        /* you can also grab the data out of the arrays*/
-        
-        
-    }
-    
-}
+//void ofApp::audioRequested     (float * output, int bufferSize, int nChannels){
+//
+//    for (int i = 0; i < bufferSize; i++){
+//
+//        /* Stick your maximilian 'play()' code in here ! Declare your objects in testApp.h.
+//
+//         For information on how maximilian works, take a look at the example code at
+//
+//         http://www.maximilian.strangeloop.co.uk
+//
+//         under 'Tutorials'.
+//
+//         */
+//
+//
+//
+////        sample=beat.play(0.25, 0, beat.length);
+//        wave=sine1.sinebuf(abs(mouseX));/* mouse controls sinewave pitch. we get abs value to stop it dropping
+//                                         //                                         delow zero and killing the soundcard*/
+//
+//        mymix.stereo(sample + wave, outputs, 0.5);
+//
+//
+//        output[i*nChannels    ] = outputs[0]; /* You may end up with lots of outputs. add them here */
+//        output[i*nChannels + 1] = outputs[1];
+//    }
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::audioReceived     (float * input, int bufferSize, int nChannels){
+//
+//
+//    /* You can just grab this input and stick it in a double, then use it above to create output*/
+//
+//    for (int i = 0; i < bufferSize; i++){
+//
+//        /* you can also grab the data out of the arrays*/
+//
+//
+//    }
+//
+//}
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
--- src/ofApp.h
+++ src/ofApp.h
@@ -21,17 +21,28 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     
-        void audioRequested     (float * input, int bufferSize, int nChannels); /* output method */
-        void audioReceived     (float * input, int bufferSize, int nChannels); /* input method */
+    
+        ofSoundPlayer         beat;
+        static constexpr size_t nBandsToGet = 128;
+        std::array<float, nBandsToGet> fftSmoothed{{0}};
+    
+        static const int rollingAvgSize = 75;
+        float rollingAvgArr[rollingAvgSize] = { 0.0 };
+        float rollingAvgArr2[rollingAvgSize] = { 0.0 };
 
-        int        initialBufferSize; /* buffer size */
-        int        sampleRate;
+    
+    
+//        void audioRequested     (float * input, int bufferSize, int nChannels); /* output method */
+//        void audioReceived     (float * input, int bufferSize, int nChannels); /* input method */
+////
+//        int        initialBufferSize; /* buffer size */
+//        int        sampleRate;
 
 
         /* stick you maximilian stuff below */
-
-        double wave,sample,outputs[2];
-        ofxMaxiMix mymix;
-        ofxMaxiOsc sine1;
-        ofxMaxiSample beats,beat;
+//
+//        double wave,sample,outputs[2];
+//        ofxMaxiMix mymix;
+//        ofxMaxiOsc sine1;
+//        ofxMaxiSample beats,beat;
 };
